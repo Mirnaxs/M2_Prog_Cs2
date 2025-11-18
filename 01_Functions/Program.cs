83119c7b1cd886162f1f0e66a70dc44274aaed84
Marnix <marnixvanderwerff@gmail.com>
2025-11-18 12:26:18 +0100

    Test?

---
+++ 01_Functions/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _01_Functions
+{
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        // Hier wordt de nieuwe variable "program" gemaakt.
+        program.Run();
+    }
+}
+}

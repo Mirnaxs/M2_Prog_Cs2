2bc12f27d9105d50d0c84ed39e080e2138087be1
Marnix <marnixvanderwerff@gmail.com>
2025-11-25 13:53:23 +0100

    02

---
+++ 02_classes/Rijschool/program.cs
@@ -0,0 +1,14 @@
+namespace _02_classes;
+
+class Program
+{
+        Auto auto1 = new Auto()
+        {
+            automaat = true,
+            kenteken = "AA824H",
+            kilometerStand= 20567,
+            merk= "Volkswagen"
+        };
+
+        
+}
---
+++ 03_Constructors/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _03_Constructors;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
